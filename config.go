8e348d2045a983c87f8d3e973ccdd2dd176a35b2
和田皓翔 <wadahiroka@wadaakirashounoMacBook-Pro.local>
2023-04-12 14:46:45 +0900

    made config.toml to set variables

--- config.go
+++ config.go
@@ -2,14 +2,22 @@
 package config
 
 import (
-	"encoding/json"
-	"io/ioutil"
-	"log"
+	"fmt"
+	"github.com/BurntSushi/toml"
 )
 
 type Config struct {
-	Port string
-	Db   string
-	Host string
-	User string
+	Setting setting
+	URL     url
+}
+type setting struct {
+	APIKey       string "toml:'SecretAPI_key'"
+	Version      string "toml:'version'"
+	Record_Limit int    "toml:'record_limit'"
+}
+
+type url struct {
+	Whisper_URL  string "toml:'whisper_URL'"
+	GPT3_URL     string "toml:'gpt3_URL'"
+	VOICEBOX_URL string "toml:'VOICEVOX_URL'"
 }
--- main.go
+++ main.go
@@ -2,20 +2,31 @@ package main
 
 import (
 	"fmt"
+	"github.com/wadda0714/My_AI_Assistant/config"
 	"os/exec"
 	"time"
+
+	"github.com/docker/docker/libnetwork/config"
 )
 
 func main() {
+	var config config.Config
+	_, err := toml.DecodeFile("config.toml", &config)
+	if err != nil {
+		fmt.Println("failed to read config.toml due to " + err)
+
+		return
+	}
+
 	fmt.Println("Please enter your voice...")
 	cmd := exec.Command("rec", "--encoding", "signed-integer", "--bits", "16", "--channels", "1", "--rate", "16000", "--endian", "little", "--type", "wav", "test.wav")
 
 	//5 seconds to record
 	cmd.Start()
-	time.Sleep(5 * time.Second)
+	time.Sleep(config.Setting.Record_Limit * time.Second)
 	cmd.Process.Kill()
 	//Send to Whisper AI with curl
-	fmt.Println("Sending to Whisper AI...")
+	fmt.Println("Sending to Whisper AI...(" + config.URL.Whisper_URL + ")")
 
 	//Create data to send to Whisper AI with curl
 	//curl -X POST -H "Content-Type: application/json" -d '{"audio": "test.wav"}' http://localhost:5000/recognize
