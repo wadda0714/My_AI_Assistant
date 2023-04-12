4adb9c07ff0e5eedce4df76dbc089b8fdaadcc12
和田皓翔 <wadahiroka@wadaakirashounoMacBook-Pro.local>
2023-04-12 16:53:05 +0900

    test

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 
 func main() {
 	var config cfg.Config
+	fmt.Println("Reading config.toml...")
 	_, err := toml.DecodeFile("config.toml", &config)
 	if err != nil {
 		fmt.Println("failed to read config.toml")
@@ -22,9 +23,10 @@ func main() {
 	fmt.Println("Please enter your voice...")
 	cmd := exec.Command("rec", "--encoding", "signed-integer", "--bits", "16", "--channels", "1", "--rate", "16000", "--endian", "little", "--type", "wav", "test.wav")
 
-	//5 seconds to record
+	//time.Duration(config.Setting.Record_Limit) seconds to record
+
 	cmd.Start()
-	time.Sleep(config.Setting.Record_Limit * time.Second)
+	time.Sleep(time.Duration(config.Setting.Record_Limit) * time.Second)
 	cmd.Process.Kill()
 	//Send to Whisper AI with curl
 	fmt.Println("Sending to Whisper AI...(" + config.URL.Whisper_URL + ")")
